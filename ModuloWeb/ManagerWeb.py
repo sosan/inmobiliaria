5725c82570442cbff6ae574f5337edfe39e35cb9
jose <josloe@gmail.com>
2020-01-16 09:55:36 +0100

    fixed gps

--- ModuloWeb/ManagerWeb.py
+++ ModuloWeb/ManagerWeb.py
@@ -11,6 +11,7 @@ class ManagerWeb:
         self.web = HTMLSession()
 
     def getstreet(self, x, y):
+        "https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=k-EckgQuyQOTGQoMy54SsslKuX9oMP8PFj2SyV-_wJM&mode=retrieveAddresses&prox=39.6156533,2.7755254&maxresults=1"
         uri = """https://reverse.geocoder.ls.hereapi.com/6.2/reversegeocode.json?apiKey=k-EckgQuyQOTGQoMy54SsslKuX9oMP8PFj2SyV-_wJM&mode=retrieveAddresses&prox={0},{1}&maxresults=1""".format(x, y)
 
         resultado = self.web.get(uri)
@@ -18,11 +19,15 @@ class ManagerWeb:
             temp = json.loads(resultado.text)
             fulldireccion = temp["Response"]["View"][0]["Result"][0]["Location"]["Address"]["Label"]
 
-            splitdire = fulldireccion.split(" ")
-            if len(splitdire) == 5:  # sin nombre de la calle
-                # ['07001', 'Palma', '(Illes', 'Balears),', 'Espanya']
-                return "Ninguna", "Ninguna", splitdire[0], splitdire[1]  # calle= None, numero=None, cp, ciudad
+            splitdire = fulldireccion.split(",")
+
+            if len(splitdire) == 2:  # sin nombre de la calle
+                # ['07001 Palma (Illes Balears)', 'Espanya']
+                separacion = splitdire[0].split(" ")
+                return "Ninguna", "Ninguna", separacion[0], separacion[1]  # calle= None, numero=None, cp, ciudad
             else:
-                return None, None, splitdire[0], splitdire[1]  # calle= None, numero=None, cp, ciudad
+                #  _____ XXX XXXXXX, 5, 00000 XXXXXX (OOOO OOOOO), LLLLLLL
+                separacion = splitdire[2].split(" ")
+                return splitdire[0].split(" ")[1:], splitdire[1], separacion[0], separacion[1]  # calle= None, numero=None, cp, ciudad
 
         return None
